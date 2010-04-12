b54e165e35ac2ab9f0d5e3335469ee1db7bc6be9
László Veréb <vereb.laszlo.86@gmail.com>
2010-04-12 15:33:19 +0200

    Kész a program, de még le kell ellenőrizni debian környezetben

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/**
+ * @file main.c
+ * @author László Veréb
+ * @date 2010.04.12.
+ */
+
 #include "match.h"
 #include "util.h"
 #include "generator.h"
@@ -8,16 +14,6 @@ double pi = M_PI;
 
 extern char * program_invocation_short_name;
 extern char * program_invocation_name;
-
-typedef struct waves_Tag{
-	long length;
-	double *data;
-} waves;
-
-void malloc_Waves(long len, waves * wave) {
-	wave->length = len;
-	wave->data = malloc(wave->length * sizeof(double));
-}
 
 int main(int argc, char *argv[]) {
 	char mode[30];
@@ -34,18 +30,17 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 	// preinicialization
-//	long max_Length = atoi(argv[1]);
-	FILE *file = sfopen_read(argv[1]);												// deallocated line: 124
+	FILE *file = sfopen_read(argv[1]);								// deallocated at line: 43
 	fscanf(file, "%d\n", &num_Detectors);
 	typedef char type_name[20];
-	type_name *t_names = malloc(num_Detectors * sizeof(type_name));					// deallocated line: 153
-	type_name *s_names = malloc(num_Detectors * sizeof(type_name));					// deallocated line: 154
-	type_name *n_names = malloc(num_Detectors * sizeof(type_name));					// deallocated line: 155
-	type_name *d_names = malloc(num_Detectors * sizeof(type_name));					// deallocated line: 174
+	type_name *t_names = malloc(num_Detectors * sizeof(type_name));	// deallocated at line: 74
+	type_name *s_names = malloc(num_Detectors * sizeof(type_name));	// deallocated at line: 75
+	type_name *n_names = malloc(num_Detectors * sizeof(type_name));	// deallocated at line: 76
+	type_name *d_names = malloc(num_Detectors * sizeof(type_name));	// deallocated at line: 93
 	for (i = 0; !feof(file); i++) {
 		fscanf(file, "%s %s %s %s\n", d_names[i], t_names[i], s_names[i], n_names[i]);
 	}
-	fclose(file);
+	fclose(file);													// allocated at line: 33
 	init_generator(mode, &dt, &freq_Min, &freq_Max);
 	program_params params = init_program();
 	long max_Length = (long) (params.max_time / dt);
@@ -55,39 +50,39 @@ int main(int argc, char *argv[]) {
 	double templates[num_Detectors][max_Length];
 	double signals[num_Detectors][max_Length];
 	double noises[num_Detectors][max_Length];
-	// allocating memory for data and reading it from files
+	// reading data from the files
 	for (i = 0; i < num_Detectors; i++) {
-		file = sfopen_read(t_names[i]);												// deallocated line: 137
+		file = sfopen_read(t_names[i]);								// deallocated at line: 60
 		for (j = 0; !feof(file); j++) {
 			fscanf(file, "%*g %lg\n", &templates[i][j]);
 		}
 		template_Length[i] = j;
-		fclose(file);
-		file = sfopen_read(s_names[i]);												// deallocated line: 144
+		fclose(file);												// allocated at line: 55
+		file = sfopen_read(s_names[i]);								// deallocated at line: 66
 		for (j = 0; !feof(file); j++) {
 			fscanf(file, "%*g %lg\n", &signals[i][j]);
 		}
 		signal_Length[i] = j;
-		fclose(file);
-		file = sfopen_read(n_names[i]);												// deallocated line: 151
+		fclose(file);												// allocated at line: 61
+		file = sfopen_read(n_names[i]);								// deallocated at line: 72
 		for (j = 0; !feof(file); j++) {
 			fscanf(file, "%*g %lg\n", &noises[i][j]);
 		}
 		noise_Length[i] = j;
-		fclose(file);
+		fclose(file);												// allocated at line: 67
 	}
-	free(t_names);
-	free(s_names);
-	free(n_names);
+	free(t_names);													// allocated at line: 36
+	free(s_names);													// allocated at line: 37
+	free(n_names);													// allocated at line: 38
 	// find the shortest length
-	detector_Struct *det = malloc(num_Detectors * sizeof(detector_Struct));						// deallocated line: end
+	detector_Struct *det = malloc(num_Detectors * sizeof(detector_Struct));	// deallocated at line: end
 	long length = fmin(fmin(template_Length[0], signal_Length[0]),noise_Length[0]);
 	for (i = 1; i < num_Detectors; i++) {
 		length = fmin(fmin(template_Length[i], signal_Length[i]), fmin(noise_Length[i], length));
 	}
 	//	fill the detector structures
 	for (i = 0; i < num_Detectors; i++) {
-		multi_Malloc(length, &det[i]);												// deallocated line: 221
+		multi_Malloc(length, &det[i]);								// deallocated at line: 140
 		det[i].det = con_Det_Str_Enum(d_names[i]);
 		for (j = 0; j < length; j++) {
 			det[i].t[j] = templates[i][j];
@@ -95,14 +90,14 @@ int main(int argc, char *argv[]) {
 			det[i].n[j] = noises[i][j];
 		}
 	}
-	free(d_names);
+	free(d_names);													// allocated at line: 39
 	// execute the Fourier algorithm
 	for (i = 0; i < num_Detectors; i++) {
 		fftw_execute(det[i].pt);
 		fftw_execute(det[i].ps);
 		fftw_execute(det[i].pn);
 	}
-	// calulates the others
+	// inicialize the input vectors for the experimental match
 	long num_Match = num_Detectors;
 	calculate_After(0, &det, &num_Match);
 	// find the frequency band
@@ -126,11 +121,11 @@ int main(int argc, char *argv[]) {
 	int good[num_Match];
 	int count[num_Match];
 	for (i = 0; i < num_Match - 1; i++) {
-		double *norm = psd(det[i].n, det[i].length, dt, blackman);					// deallocated line: 209
+		double *norm = psd(det[i].n, det[i].length, dt, blackman);	// deallocated at line: 128
 		double ts = scalar_freq(det[i].ct, det[i].cs, norm, minfr, maxfr);
 		double tt = scalar_freq(det[i].ct, det[i].ct, norm, minfr, maxfr);
 		double ss = scalar_freq(det[i].cs, det[i].cs, norm, minfr, maxfr);
-		fftw_free(norm);
+		fftw_free(norm);											// allocated at line: 124
 		if (i < num_Detectors) {
 			ts_Sum += ts;
 			tt_Sum += tt;
@@ -142,19 +137,19 @@ int main(int argc, char *argv[]) {
 	own_Match[0] = ts_Sum / sqrt(tt_Sum * ss_Sum);
 	count[0] = 0;
 	for (i = 0; i < num_Match - 1; i++) {
-		multi_Free(det[i]);
+		multi_Free(det[i]);											// allocated at line: 85
 	}
 	// write the own match to the file
-	file = sfopen_write("own.match");												// deallocated line: 231
+	file = sfopen_write("own.match");								// deallocated at line: 149
 	for (i = 0; i < num_Match; i++) {
 		fprintf(file, "%15.10lg ", own_Match[i]);
 		printf("%15.10lg ", own_Match[i]);
 	}
 	printf("\n");
-	fclose(file);
+	fclose(file);													// allocated at line: 143
 	// Initialize output files
-/*	{
-		file = sfopen_write("parameters.data");										// deallocated line: 248
+	{
+		file = sfopen_write("parameters.data");						// deallocated at line: 166
 		fprintf(file, "#%7s %13s %13s ", "index", "m1", "m2");
 		fprintf(file, "%13s %13s %13s ", "s1x", "s1y", "s1z" );
 		fprintf(file, "%13s %13s %13s ", "s2x", "s2y", "s2z" );
@@ -168,15 +163,15 @@ int main(int argc, char *argv[]) {
 		fprintf(file, "%13s %13s %13s %13s ", "han_match", "liv_match", "hl_match", "cor_match");
 		fprintf(file, "%3s %3s %3s %3s ", "h", "l", "hl", "c");
 		fprintf(file, "%13s %13s %13s %13s\n", "han_dev", "liv_dev", "hl_dev", "cor_dev");
-		fclose(file);
-		file = sfopen_write("to_Plot.data");										// deallocated line: 255
+		fclose(file);												// allocated at line: 152
+		file = sfopen_write("to_Plot.data");						// deallocated at line: 173
 		fprintf(file, "#%7s %13s %7s ", "index", "match", "dev");
 		fprintf(file, "%13s %13s ", "m1", "m2");
 		fprintf(file, "%12s %12s ", "sp1", "sp2");
 		fprintf(file, "%12s %12s ", "ph1", "th1");
 		fprintf(file, "%12s %12s\n", "ph2", "th2");
-		fclose(file);
-	}*/
+		fclose(file);												// allocated at line: 167
+	}
 	srand(time(NULL));
 	// starting the loop
 	long gen, all_m, bad_m, good_m;
@@ -188,18 +183,19 @@ int main(int argc, char *argv[]) {
 		sprintf(LALparams, "./lal %30.20lg %30.20lg %30.20lg %30.20lg %30.20lg %30.20lg %30.20lg\
 			%30.20lg %30.20lg %3lg %3lg %3lg %30.20lg %s gen.out", now.bh1.m, now.bh2.m,
 			now.bh1.sx, now.bh1.sy, now.bh1.sz, now.bh2.sx, now.bh2.sy, now.bh2.sz, now.incl, freq_Min, freq_Max, now.dist, dt, mode);
-//		system(LALparams);
+		system(LALparams);
 		errno = 0;
-		file = fopen ("noise.wave", "r");												// deallocated line: 294
+		file = fopen ("gen.out", "r");								// deallocated at line: 212
 		if (file == NULL) {
 			fprintf (stderr, "%s: Couldn't open file for reading %s; %s\n", program_invocation_short_name, "gen.out", strerror (errno));
 			fprintf (stderr, "Skipping!\n");
 			continue;
 		}
 		gen++;
+		// if the waveform generation was succes, read the file
 		double * fp, * fc;
-		fp = malloc(num_Detectors * sizeof(double));											// deallocated line: 295
-		fc = malloc(num_Detectors * sizeof(double));											// deallocated line: 296
+		fp = malloc(num_Detectors * sizeof(double));				// deallocated at line: 213
+		fc = malloc(num_Detectors * sizeof(double));				// deallocated at line: 214
 		for (j = 0; j < num_Detectors; j++) {
 			calc_Response_For_Detector(det[j].det, now.dec, now.phi, now.pol, &fp[j], &fc[j]);
 		}
@@ -213,21 +209,21 @@ int main(int argc, char *argv[]) {
 			}
 			length++;
 		}
-		fclose(file);
-		free(fp);
-		free(fc);
+		fclose(file);												// allocated at line: 188
+		free(fp);													// allocated at line: 197
+		free(fc);													// allocated at line: 198
 		if (dt * length < params.min_time) {
 			continue;
 		}
 		all_m++;
+		// if it is at least the minimum length, we use it
 		length = fmin(fmin(template_Length[0], signal_Length[0]),noise_Length[0]);
 		for (j = 0; j < num_Detectors; j++) {
 			length = fmin(fmin(template_Length[j], signal_Length[j]), fmin(noise_Length[j], length));
 		}
 		//	fill the detector structures
 		for (j = 0; j < num_Detectors; j++) {
-			multi_Malloc(length, &det[j]);												// deallocated line: 353
-			//	Már meg van adva, melyik detektor
+			multi_Malloc(length, &det[j]);							// deallocated at line: 271
 			for (k = 0; k < length; k++) {
 				det[j].t[k] = templates[j][k];
 				det[j].s[k] = signals[j][k];
@@ -240,11 +236,9 @@ int main(int argc, char *argv[]) {
 			fftw_execute(det[j].ps);
 			fftw_execute(det[j].pn);
 		}
-		// calulates the others
+		// inicialize the input vectors for the experimental match
 		num_Match = num_Detectors;
-//printf("calc %ld\n", num_Match);
 		calculate_After(i, &det, &num_Match);
-//printf("calc %ld\n", num_Match);
 		// find the frequency band
 		fr = 0.;
 		freq_Step = 1. / (dt * length);
@@ -260,11 +254,11 @@ int main(int argc, char *argv[]) {
 		// calculating the match
 		ts_Sum = 0., tt_Sum = 0., ss_Sum = 0.;
 		for (j = 0; j < num_Match; j++) {
-			double *norm = psd(det[j].n, det[j].length, dt, blackman);					// deallocated line: 343
+			double *norm = psd(det[j].n, det[j].length, dt, blackman);// deallocated at line: 261
 			double ts = scalar_freq(det[j].ct, det[j].cs, norm, minfr, maxfr);
 			double tt = scalar_freq(det[j].ct, det[j].ct, norm, minfr, maxfr);
 			double ss = scalar_freq(det[j].cs, det[j].cs, norm, minfr, maxfr);
-			fftw_free(norm);
+			fftw_free(norm);										// allocated at line: 257
 			if (j < num_Detectors) {
 				ts_Sum += ts;
 				tt_Sum += tt;
@@ -274,7 +268,7 @@ int main(int argc, char *argv[]) {
 		}
 		match[0] = ts_Sum / sqrt(tt_Sum * ss_Sum);
 		for (j = 0; j < num_Match; j++) {
-			multi_Free(det[j]);
+			multi_Free(det[j]);										// allocated at line: 226
 		}
 		for (j = 0; j < num_Match; j++) {
 			double temp1 = own_Match[j] * (1. - params.deviation);
@@ -295,32 +289,29 @@ int main(int argc, char *argv[]) {
 		for (j = 0; j < num_Detectors; j++) {
 			all_good += good[j];
 		}
-//		if (all_good == 0) {
-		if (false) {
-			//	FELSZABADíTANI MEMÓRIÁT	XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+		if (all_good == 0) {
 			continue;
 		}
-		multi_Malloc(det[0].length * 2, &det[num_Match - 1]);								// deallocated line: 399
-//		printf("Correlation: ");
-//		fflush(stdout);
-//		calc_Time_Corr(det[1].t, det[0].t, det[num_Match].t, det[num_Match].length / 2);
-//		printf("h ");
-//		fflush(stdout);
-//		calc_Time_Corr(det[1].s, det[0].s, det[num_Match].s, det[num_Match].length / 2);
-//		printf("s ");
-//		fflush(stdout);
-//		calc_Time_Corr(det[1].n, det[0].n, det[num_Match].n, det[num_Match].length / 2);
-//		printf("n\n");
-//		fflush(stdout);
-//		fftw_execute(det[num_Match].pt);
-//		fftw_execute(det[num_Match].ps);
-		double *norm = psd(det[num_Match - 1].n, det[num_Match - 1].length, dt, blackman);					// deallocated line: 396
-//		double ts = scalar_freq(det[num_Match].ct, det[num_Match].cs, norm, minfr, maxfr);
-//		double tt = scalar_freq(det[num_Match].ct, det[num_Match].ct, norm, minfr, maxfr);
-//		double ss = scalar_freq(det[num_Match].cs, det[num_Match].cs, norm, minfr, maxfr);
-		double ts = 1., tt = 1., ss = 1.;	//segéd
-		fftw_free(norm);
-		multi_Free(det[num_Match - 1]);
+		multi_Malloc(det[0].length * 2, &det[num_Match - 1]);		// deallocated at line: 314
+		printf("Correlation: ");
+		fflush(stdout);
+		calc_Time_Corr(det[1].t, det[0].t, det[num_Match].t, det[num_Match].length / 2);
+		printf("h ");
+		fflush(stdout);
+		calc_Time_Corr(det[1].s, det[0].s, det[num_Match].s, det[num_Match].length / 2);
+		printf("s ");
+		fflush(stdout);
+		calc_Time_Corr(det[1].n, det[0].n, det[num_Match].n, det[num_Match].length / 2);
+		printf("n\n");
+		fflush(stdout);
+		fftw_execute(det[num_Match].pt);
+		fftw_execute(det[num_Match].ps);
+		double *norm = psd(det[num_Match - 1].n, det[num_Match - 1].length, dt, blackman);// deallocated at line: 314
+		double ts = scalar_freq(det[num_Match].ct, det[num_Match].cs, norm, minfr, maxfr);
+		double tt = scalar_freq(det[num_Match].ct, det[num_Match].ct, norm, minfr, maxfr);
+		double ss = scalar_freq(det[num_Match].cs, det[num_Match].cs, norm, minfr, maxfr);
+		fftw_free(norm);											// allocated at line: 309
+		multi_Free(det[num_Match - 1]);								// allocated at line: 295
 		num_Match++;
 		match[num_Match] = ts / sqrt(tt * ss);
 		double temp1 = own_Match[num_Match] * (1. - params.deviation);
@@ -345,18 +336,18 @@ int main(int argc, char *argv[]) {
 		}
 		if (all_good == 1) {
 			good_m++;
-/*			write_Waveh(i, templates[0], length, dt);
+			write_Waveh(i, templates[0], length, dt);
 			write_Wavel(i, templates[1], length, dt);
 			write_Gen_Data(i, freq_Min, freq_Max, match, diff, good, num_Match, &now, dt);
-			write_Data_to_Plot(i, match, diff, good, num_Match, &now);*/
+			write_Data_to_Plot(i, match, diff, good, num_Match, &now);
 		} else {
 			bad_m++;
-/*			write_Waveh(bad_m, templates[0], length, dt);
+			write_Waveh(bad_m, templates[0], length, dt);
 			write_Wavel(bad_m, templates[1], length, dt);
 			write_Gen_Datax(bad_m, freq_Min, freq_Max, match, diff, good, num_Match, &now, dt);
-			write_Data_to_Plotx(bad_m, match, diff, good, num_Match, &now);*/
+			write_Data_to_Plotx(bad_m, match, diff, good, num_Match, &now);
 		}
-		if (i % 100 == 0) {
+//		if (i % 100 == 0) {
 			printf("\nindex: %ld, gen: %ld, all_m: %ld, bad_m: %ld, good_m: %ld\n", i, gen, all_m, bad_m, good_m);
 			printf("sum, detecotrs, fft_corr, time_corr: ");
 			for (j = 0; j <= num_Match; j++) {
@@ -364,16 +355,16 @@ int main(int argc, char *argv[]) {
 			}
 			printf("\n");
 			fflush(stdout);
-		}
-		if (i - 1> 0 && i % 10 == 0) {
+//		}
+		if (i - 1 > 0 && i % 10 == 0) {
 			printf("[%ld %ld]", i, gen);
 			fflush(stdout);
 		}
 	}
 	// freeing things
-	free(templates);
-	free(signals);
-	free(noises);
-	free(det);
+//	free(templates);
+//	free(signals);
+//	free(noises);
+	free(det);														// allocated at line: 78
 	return 0;
 }
