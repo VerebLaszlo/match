a3e7d92309b31ef565770d5c7db0f1e14c52a699
László Veréb <vereb.laszlo.86@gmail.com>
2010-10-27 13:01:55 +0200

    Reproduction maked easier.
    The used parameters should be printed before the corresponding waveform
    by the print_Binary_System() function. Then the waveforms can be printed
    with the sqt-st_loop.gpl.sample gnuplot script.

--- generator.c
+++ generator.c
@@ -10,6 +10,7 @@
 void init_Binary_System(binary_System *min, binary_System *max) {
 	memset(min, 0, sizeof(binary_System));
 	memset(max, 0, sizeof(binary_System));
+	min->coaTime = max->coaTime = 0.;
 	min->incl = DBL_MIN;
 	short i;
 	for (i = 0; i < 2; i++) {
@@ -28,38 +29,57 @@ void init_Binary_System(binary_System *min, binary_System *max) {
 	max->eta = 0.25;
 	max->coaPhase = 2. * M_PI;
 	max->incl = 2. * M_PI; ///< \bug nem biztos
+	max->F.alpha = max->F.dec = max->F.pol = 2. * M_PI;
+	min->F.gmst = DBL_MIN;
+	max->F.gmst = DBL_MAX;
 }
 
-void print_Binary_System(binary_System *sys, FILE *stream) {
-	short i;
-	fprintf(stream, "System:\n");
-	fprintf(stream, "M_C="PREC"M="PREC"eta="PREC"phi_C="PREC"i="PREC"d_L="PREC"\n",
-			sys->chirpM, sys->M, sys->eta, sys->coaPhase, sys->incl, sys->dist);
-	for (i = 0; i < 2; i++) {
-		fprintf(stream, "Black Hole %d:\n", i + 1);
-		fprintf(
-				stream,
-				"m="PREC"th="PREC"vphi="PREC"phi="PREC"kappa="PREC"psi="PREC"ctheta="PREC"chi_Amp="PREC"chi_x="PREC"chi_y="PREC"chi_z="PREC"\n",
-				sys->bh[i].m, sys->bh[i].theta, sys->bh[i].varphi,
-				sys->bh[i].phi, sys->bh[i].kappa, sys->bh[i].psi,
-				sys->bh[i].ctheta, sys->bh[i].chi_Amp, sys->bh[i].chi[0],
-				sys->bh[i].chi[1], sys->bh[i].chi[2]);
-	}
+void print_Binary_System(binary_System *sys, program_Params *params,
+		FILE *stream) {
 	fprintf(
 			stream,
-			"dec="PREC"pol="PREC"right_ascension="PREC"gmst="PREC"F_+="PREC"F_x="PREC"\n",
-			sys->F.dec, sys->F.pol, sys->F.alpha, sys->F.gmst,
-			sys->F.F[0], sys->F.F[1]);
-	fprintf(stream, "===========================\n");fflush(stream);
+			"#........index,f_I,f_F,f_S,t_S,F+,Fx: % -10ld"PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL"\n",
+			params->index, params->freq_Initial, params->freq_Final,
+			params->freq_Sampling, params->time_Sampling, sys->F.F[0],
+			sys->F.F[1]);
+	fprintf(
+			stream,
+			"#...........M_{Chirp},M,eta,mu,m1,m2: "PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL"\n",
+			sys->chirpM, sys->M, sys->eta, sys->mu, sys->bh[0].m, sys->bh[1].m);
+	fprintf(
+			stream,
+			"#....chi1,theta1,varphi1,kappa1,psi1: "PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL"\n",
+			sys->bh[0].chi_Amp, sys->bh[0].theta, sys->bh[0].varphi,
+			sys->bh[0].kappa, sys->bh[0].psi);
+	fprintf(
+			stream,
+			"#....chi2,theta2,varphi2,kappa2,psi2: "PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL"\n",
+			sys->bh[1].chi_Amp, sys->bh[1].theta, sys->bh[1].varphi,
+			sys->bh[1].kappa, sys->bh[1].psi);
+	fprintf(
+			stream,
+			"#.incl,d_L,t_C,phi_C,dec,pol,ra,gmst: "PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL"\n",
+			sys->incl, sys->dist, sys->coaTime, sys->coaPhase, sys->F.dec,
+			sys->F.pol, sys->F.alpha, sys->F.gmst);
+	fprintf(
+			stream,
+			"#chi1x,chi1y,chi1z,chi2x,chi2y,chi2z: "PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL"\n",
+			sys->bh[0].chi[0], sys->bh[0].chi[1], sys->bh[0].chi[2],
+			sys->bh[1].chi[0], sys->bh[1].chi[1], sys->bh[1].chi[2]);
+	fprintf(
+			stream,
+			"#...typ,typT,best,worst,bestT,worstT: "PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL PREC_PL"\n",
+			params->match_Typ, params->match_TypT, params->match_Best,
+			params->match_Worst, params->match_BestT, params->match_WorstT);
+	fprintf(
+			stream,
+			"#............period1,period2,periodD: "PREC_PL PREC_PL PREC_PL"\n",
+			params->periods[0], params->periods[1], params->periodsD);
+	fflush(stream);
 }
 
 void convert_Spins(binary_System *sys, conversion_Mode_Spins mode) {
 	short i;
-	if (mode == FROM_CTHETA_VPHI || mode == FROM_CTHETA_PHI) {
-		sys->bh[0].theta = acos(sys->bh[0].ctheta);
-		sys->bh[1].theta = acos(sys->bh[1].ctheta);
-		mode--;
-	}
 	switch (mode) {
 		case FROM_XYZ:
 			for (i = 0; i < 2; i++) {
@@ -87,9 +107,9 @@ void convert_Spins(binary_System *sys, conversion_Mode_Spins mode) {
 						* sin(sys->bh[i].varphi);
 				sys->bh[i].chi[2] = sys->bh[0].chi_Amp * cos(sys->bh[i].theta);
 				sys->bh[i].phi = sys->bh[i].varphi + M_PI / 2.; ///< kihasználva, hogy \f$\hat{L_N} x-z síkban van, álltalánosítani kell\f$
-				sys->bh[i].kappa = acos(sin(sys->bh[i].theta) * sin(
-						sys->bh[i].phi) * sin(sys->incl)
-						+ cos(sys->bh[i].theta) * cos(sys->incl));
+				sys->bh[i].kappa = acos(sin(sys->bh[i].theta) * cos(
+						sys->bh[i].varphi) * sin(sys->incl) + cos(
+						sys->bh[i].theta) * cos(sys->incl));
 				sys->bh[i].psi = atan(tan(sys->bh[i].phi) * cos(sys->incl)
 						- sin(sys->incl) / (cos(sys->bh[i].phi) * tan(
 								sys->bh[i].theta)));
@@ -97,14 +117,14 @@ void convert_Spins(binary_System *sys, conversion_Mode_Spins mode) {
 			break;
 		case FROM_THETA_PHI:
 			for (i = 0; i < 2; i++) {
+				sys->bh[i].varphi = sys->bh[i].phi - M_PI / 2.; ///< kihasználva, hogy \f$\hat{L_N} x-z síkban van, álltalánosítani kell\f$
 				sys->bh[i].ctheta = cos(sys->bh[i].theta);
-				sys->bh[i].kappa = acos(sin(sys->bh[i].theta) * sin(
-						sys->bh[i].phi) * sin(sys->incl)
-						+ cos(sys->bh[i].theta) * cos(sys->incl));
+				sys->bh[i].kappa = acos(sin(sys->bh[i].theta) * cos(
+						sys->bh[i].varphi) * sin(sys->incl) + cos(
+						sys->bh[i].theta) * cos(sys->incl));
 				sys->bh[i].psi = atan(tan(sys->bh[i].phi) * cos(sys->incl)
 						- sin(sys->incl) / (cos(sys->bh[i].phi) * tan(
 								sys->bh[i].theta)));
-				sys->bh[i].varphi = sys->bh[i].phi - M_PI / 2.; ///< kihasználva, hogy \f$\hat{L_N} x-z síkban van, álltalánosítani kell\f$
 				sys->bh[i].chi[0] = sys->bh[0].chi_Amp * sin(sys->bh[i].theta)
 						* cos(sys->bh[i].varphi);
 				sys->bh[i].chi[1] = sys->bh[0].chi_Amp * sin(sys->bh[i].theta)
@@ -143,16 +163,22 @@ void convert_Masses(binary_System *sys, conversion_Mode_Masses mode) {
 			sys->bh[0].m = (1. + sqrt(1. - 4. * sys->eta)) * sys->M / 2.;
 			sys->bh[1].m = (1. - sqrt(1. - 4. * sys->eta)) * sys->M / 2.;
 			sys->chirpM = pow(sys->eta, 3. / 5.) * sys->M;
+			sys->mu = sys->bh[0].m > sys->bh[1].m ? sys->bh[0].m / sys->bh[1].m
+					: sys->bh[1].m / sys->bh[0].m;
 			break;
 		case FROM_M1M2:
 			sys->M = sys->bh[0].m + sys->bh[1].m;
 			sys->eta = sys->bh[0].m * sys->bh[1].m / SQR(sys->M);
 			sys->chirpM = pow(sys->eta, 3. / 5.) * sys->M;
+			sys->mu = sys->bh[0].m > sys->bh[1].m ? sys->bh[0].m / sys->bh[1].m
+					: sys->bh[1].m / sys->bh[0].m;
 			break;
 		case FROM_ETACHIRP:
 			sys->M = sys->chirpM / pow(sys->eta, 3. / 5.);
 			sys->bh[0].m = (1. + sqrt(1. - 4. * sys->eta)) * sys->M / 2.;
 			sys->bh[1].m = (1. - sqrt(1. - 4. * sys->eta)) * sys->M / 2.;
+			sys->mu = sys->bh[0].m > sys->bh[1].m ? sys->bh[0].m / sys->bh[1].m
+					: sys->bh[1].m / sys->bh[0].m;
 			break;
 		default:
 			fprintf(stderr, "Invalid mass conversion.");
@@ -173,9 +199,10 @@ void gen_Mass(binary_System *sys, binary_System *min, binary_System *max,
 				sys->eta = RANDNK(min->eta, max->eta);
 				sys->bh[0].m = (1. + sqrt(1. - 4. * sys->eta)) * sys->M / 2.;
 				sys->bh[1].m = (1. - sqrt(1. - 4. * sys->eta)) * sys->M / 2.;
-			} while (min->bh[0].m > sys->bh[0].m || sys->bh[0].m > max->bh[0].m
-					|| min->bh[1].m > sys->bh[1].m || sys->bh[1].m
-					> max->bh[1].m);
+			} while (min->bh[0].m > sys->bh[0].m || sys->bh[0].m > max->bh[0].m ||
+					 min->bh[1].m > sys->bh[1].m || sys->bh[1].m > max->bh[1].m);
+				sys->mu = sys->bh[0].m > sys->bh[1].m ? sys->bh[0].m / sys->bh[1].m
+						: sys->bh[1].m / sys->bh[0].m;
 			break;
 		case M1M2:
 			do {
@@ -185,6 +212,8 @@ void gen_Mass(binary_System *sys, binary_System *min, binary_System *max,
 				sys->eta = sys->bh[0].m * sys->bh[1].m / SQR(sys->M);
 			} while (min->M > sys->M || sys->M > max->M || min->eta > sys->eta
 					|| sys->eta > max->eta);
+			sys->mu = sys->bh[0].m > sys->bh[1].m ? sys->bh[0].m / sys->bh[1].m
+					: sys->bh[1].m / sys->bh[0].m;
 			break;
 		case ETACHRIP:
 			fprintf(stderr, "Not implemented yet.");
@@ -212,7 +241,9 @@ void gen_Chi(binary_System *sys, binary_System *min, binary_System *max,
 			for (i = 0; i < 3; i++) {
 				sys->bh[i].chi_Amp
 						= RANDNK(min->bh[i].chi_Amp, max->bh[i].chi_Amp);
-				sys->bh[i].theta = acos(RANDNK(cos(min->bh[i].theta), cos(max->bh[i].theta)));
+				sys->bh[i].theta
+						= acos(
+								RANDNK(cos(min->bh[i].theta), cos(max->bh[i].theta)));
 				sys->bh[i].varphi
 						= RANDNK(min->bh[i].varphi, max->bh[i].varphi);
 			}
@@ -222,7 +253,9 @@ void gen_Chi(binary_System *sys, binary_System *min, binary_System *max,
 			for (i = 0; i < 3; i++) {
 				sys->bh[i].chi_Amp
 						= RANDNK(min->bh[i].chi_Amp, max->bh[i].chi_Amp);
-				sys->bh[i].theta = acos(RANDNK(cos(min->bh[i].theta), cos(max->bh[i].theta)));
+				sys->bh[i].theta
+						= acos(
+								RANDNK(cos(min->bh[i].theta), cos(max->bh[i].theta)));
 				sys->bh[i].phi = RANDNK(min->bh[i].phi, max->bh[i].phi);
 			}
 			convert_Spins(sys, FROM_THETA_PHI);
@@ -231,7 +264,9 @@ void gen_Chi(binary_System *sys, binary_System *min, binary_System *max,
 			for (i = 0; i < 3; i++) {
 				sys->bh[i].chi_Amp
 						= RANDNK(min->bh[i].chi_Amp, max->bh[i].chi_Amp);
-				sys->bh[i].kappa = acos(RANDNK(cos(min->bh[i].kappa), cos(max->bh[i].kappa)));
+				sys->bh[i].kappa
+						= acos(
+								RANDNK(cos(min->bh[i].kappa), cos(max->bh[i].kappa)));
 				sys->bh[i].psi = RANDNK(min->bh[i].psi, max->bh[i].psi);
 			}
 			convert_Spins(sys, FROM_KAPPA_PSI);
@@ -251,6 +286,7 @@ void gen_Sys(binary_System *sys, binary_System *min, binary_System *max) {
 	sys->F.dec = RANDNK(min->F.dec, max->F.dec);
 	sys->F.pol = RANDNK(min->F.pol, max->F.pol);
 	sys->F.alpha = RANDNK(min->F.alpha, max->F.alpha);
+	sys->F.gmst = RANDNK(min->F.gmst, max->F.gmst);
 	calc_Response_For_Detector(LH, sys);
 }
 
@@ -259,13 +295,4 @@ void gen_Parameters(binary_System *sys, binary_System *min, binary_System *max,
 	gen_Sys(sys, min, max);
 	gen_Mass(sys, min, max, mass);
 	gen_Chi(sys, min, max, spin);
-}
-
-void calc_Response_For_Detector(detector det, binary_System *sys) {
-	double rm[3][3];
-	detector_table dettable;
-	double phi = sys->F.alpha - sys->F.gmst;
-	dettable = GetDetectorTable(det);
-	calc_Response_Matrix(dettable.nx, dettable.ny, rm);
-	calc_Response(rm, sys->F.dec, phi, sys->F.pol, &(sys->F.F[0]), &(sys->F.F[1]));
 }
--- generator.h
+++ generator.h
@@ -11,45 +11,11 @@
 #include <stdio.h>
 #include <string.h>
 #include "util_math.h"
+#include "variables.h"
 #include "detector.h"
 
 #define PREC "% -14.8lg "
-
-typedef struct {
-	double m; ///< mass in \f$M_\odot\f$
-	double theta; ///< inclination in the \f$x,y,z\f$ radiation and in \f$l,k,z\f$ frame #l=y
-	double varphi; ///< azimuth in the \f$x,y,z\f$ radiation frame
-	double phi; ///< azimuth in the \f$l,k,z\f$ frame where \f$\hat{l}=\hat{z}\times\hat{L_N}/\sin\iota\f$
-	double kappa; ///< inclination in the \f$l,m,L_N\f$ frame
-	double psi; ///< azimuth in the \f$l,m,L_N\f$ frame
-	double ctheta; ///< \f$\cos\left(\theta\right)\f$
-	double chi_Amp; ///< amplitude of the \f$\chi\f$ dimensionless spin
-	double chi[3]; ///< the componenet of the \f$\chi\f$ dimensionless spin
-} black_Hole;
-
-/**
- * cos(theta) = det_z / r
- * cos(phi) = x / (r * sin(theta))
- * psi) =
- */
-typedef struct {
-	double dec; ///< declination
-	double pol; ///< polarisation
-	double alpha; ///< right ascension
-	double gmst;
-	double F[2]; ///< antenna functions: \f$F_+, F_\times\f$
-} antenna_Func;
-
-typedef struct {
-	black_Hole bh[2]; ///< parameters of the BHs
-	double M; ///< the total mass of the system
-	double chirpM; ///< the chirp mass of the system
-	double eta; ///< symmetric mass ratio of the system
-	double incl; ///< inlination of the system
-	double dist; ///< distance of the system
-	double coaPhase;
-	antenna_Func F; ///< antenna functions
-} binary_System;
+#define PREC_PL "% -10.4lg "
 
 typedef enum {
 	FROM_XYZ = 0, FROM_THETA_VPHI, FROM_CTHETA_VPHI, FROM_THETA_PHI, FROM_CTHETA_PHI, FROM_KAPPA_PSI,
@@ -69,7 +35,8 @@ typedef enum {
 
 void init_Binary_System(binary_System *min, binary_System *max);
 
-void print_Binary_System(binary_System *sys, FILE *stream);
+void print_Binary_System(binary_System *sys, program_Params *params,
+		FILE *stream);
 
 void convert_Spins(binary_System *sys, conversion_Mode_Spins mode);
 
@@ -90,17 +57,5 @@ void gen_Sys(binary_System *sys, binary_System *min, binary_System *max);
 
 void gen_Parameters(binary_System *sys, binary_System *min, binary_System *max,
 		gen_Mode_Masses mass, gen_Mode_Spin spin);
-
-/**
- *		The function calulates the given detectors response-function. Currentli supports only three.
- *	\bug	Még be kell fejezni a többi detektorra
- * @param[in]	det	: the detectors ID from the enumeration, (LL : Livingston, LH : Hanford, VIRGO: Virgo)
- * @param[in]	dec	: the declination of the source
- * @param[in]	phi	: \todo ez mi
- * @param[in]	pol	: the sources polarization angle
- * @param[out] fp	: F+
- * @param[out] fc	: Fx
- */
-void calc_Response_For_Detector(detector det, binary_System *sys);
 
 #endif /* GENERATOR_H */
