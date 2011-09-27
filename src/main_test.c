1a3aa5105678c9e7fd8f627800d400418a4904b2
László Veréb <vereb.laszlo.86@gmail.com>
2011-09-27 19:15:43 +0200

    Simple option parser added.

--- src/main_test.c
+++ src/main_test.c
@@ -7,13 +7,64 @@
  */
 
 #include "program_functions.h"
+#include <string.h>
+#include <stdlib.h>
+
+static void printHelp(void) {
+	puts("h: prints this help");
+	puts("p: save the generated waveform for plotting");
+	puts("m: calculate the match");
+	puts("s: the file containing the parameters");
+	puts("i: the file initialize the program");
+}
+
+typedef struct {
+	char parameterFile[FILENAME_MAX];
+	char programFile[FILENAME_MAX];
+	bool plot;
+	bool calculateMatch;
+} Options;
+
+static void interpretOptions(Options *option, int argc, char *argv[]) {
+	option->plot = false;
+	option->calculateMatch = false;
+	while (argc) {
+		if ((*argv)[0] == '-') {
+			if ((*argv)[1] == 'h') {
+				printHelp();
+				exit(EXIT_SUCCESS);
+			} else if ((*argv)[1] == 'p') {
+				option->plot = true;
+			} else if ((*argv)[1] == 'm') {
+				option->calculateMatch = true;
+			} else if ((*argv)[1] == 's') {
+				argc--;
+				argv++;
+				strcpy(option->parameterFile, *argv);
+			} else if ((*argv)[1] == 'i') {
+				argc--;
+				argv++;
+				strcpy(option->programFile, *argv);
+			}
+		}
+		argc--;
+		argv++;
+	}
+}
 
 int main(int argc, char *argv[]) {
-	printf("%d: %s\n", argc, argv[0]);
 #ifdef TEST
-	testingFunctions();
+	//testingFunctions();
 #endif // TEST
-	run(argv[1], argv[2], true, false);
+	Options option;
+	if (argc == 1) {
+		printHelp();
+		exit(EXIT_SUCCESS);
+	}
+	argc--;
+	argv++;
+	interpretOptions(&option, argc, argv);
+	run(option.programFile, option.parameterFile, option.plot, option.calculateMatch);
 	puts("\nOK");
 	return 0;
 }
